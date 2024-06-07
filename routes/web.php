ad26a6037fa501374532e88f420511c6b2e0eaca
koplakzs <atom.go.76@gmail.com>
2024-06-07 15:30:06 +0700

    add product template

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(HomeController::class)->group(function () {
     Route::get("/", "index");
+    Route::get("/product", "product");
     Route::get("/contact", "contact");
     Route::get("/about", "about");
     Route::get("/service", "service");
