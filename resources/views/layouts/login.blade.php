c213aa92961b30c37dbf5497b615add40fef5706
koplakzs <atom.go.76@gmail.com>
2024-06-07 10:02:52 +0700

    fix conflict after merge

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Login Admininistrator Apotek</title>
+    <title>Login Administrator Apotek</title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
@@ -25,8 +25,9 @@
     <!-- Style.css -->
     <link rel="stylesheet" type="text/css" href="/admin/assets/css/style.css">
 </head>
+
 <body>
- <!-- Pre-loader start -->
+    <!-- Pre-loader start -->
     <div class="theme-loader">
         <div class="loader-track">
             <div class="preloader-wrapper">
@@ -78,120 +79,6 @@
         </div>
     </div>
     <!-- Pre-loader end -->
-  
-      <section class="login-block">
-          <!-- Container-fluid starts -->
-          <div class="container">
-              <div class="row">
-                  <div class="col-sm-12">
-                      <!-- Authentication card start -->
-  
-                          <form class="md-float-material form-material">
-                              <div class="auth-box card">
-                                  <div class="card-block">
-                                      <div class="row m-b-20">
-                                          <div class="col-md-12">
-                                              <h3 class="text-center">Sign In</h3>
-                                          </div>
-                                      </div>
-                                      <div class="form-group form-primary">
-                                          <input type="text" name="email" class="form-control">
-                                          <span class="form-bar"></span>
-                                          <label class="float-label">Your Email Address</label>
-                                      </div>
-                                      <div class="form-group form-primary">
-                                          <input type="password" name="password" class="form-control">
-                                          <span class="form-bar"></span>
-                                          <label class="float-label">Password</label>
-                                      </div>
-                                      <div class="row m-t-25 text-left">
-                                          <div class="col-12">
-                                              <div class="checkbox-fade fade-in-primary d-">
-                                                  <label>
-                                                      <input type="checkbox" value="">
-                                                      <span class="cr"><i class="cr-icon icofont icofont-ui-check txt-primary"></i></span>
-                                                      <span class="text-inverse">Remember me</span>
-                                                  </label>
-                                              </div>
-                                              <div class="forgot-phone text-right f-right">
-                                                  <a href="auth-reset-password.html" class="text-right f-w-600"> Forgot Password?</a>
-                                              </div>
-                                          </div>
-                                      </div>
-                                      <div class="row m-t-30">
-                                          <div class="col-md-12">
-                                              <button type="button" class="btn btn-primary btn-md btn-block waves-effect waves-light text-center m-b-20">Sign in</button>
-                                          </div>
-                                      </div>
-                                      <hr/>
-                                  </div>
-                              </div>
-                          </form>
-                          <!-- end of form -->
-                  </div>
-                  <!-- end of col-sm-12 -->
-              </div>
-              <!-- end of row -->
-          </div>
-          <!-- end of container-fluid -->
-      </section>
-      <!-- Warning Section Starts -->
-      <!-- Older IE warning message -->
-      <!--[if lt IE 10]>
-  <div class="ie-warning">
-      <h1>Warning!!</h1>
-      <p>You are using an outdated version of Internet Explorer, please upgrade <br/>to any of the following web browsers to access this website.</p>
-      <div class="iew-container">
-          <ul class="iew-download">
-              <li>
-                  <a href="http://www.google.com/chrome/">
-                      <img src="assets/images/browser/chrome.png" alt="Chrome">
-                      <div>Chrome</div>
-                  </a>
-              </li>
-              <li>
-                  <a href="https://www.mozilla.org/en-US/firefox/new/">
-                      <img src="assets/images/browser/firefox.png" alt="Firefox">
-                      <div>Firefox</div>
-                  </a>
-              </li>
-              <li>
-                  <a href="http://www.opera.com">
-                      <img src="assets/images/browser/opera.png" alt="Opera">
-                      <div>Opera</div>
-                  </a>
-              </li>
-              <li>
-                  <a href="https://www.apple.com/safari/">
-                      <img src="assets/images/browser/safari.png" alt="Safari">
-                      <div>Safari</div>
-                  </a>
-              </li>
-              <li>
-                  <a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie">
-                      <img src="assets/images/browser/ie.png" alt="">
-                      <div>IE (9 & above)</div>
-                  </a>
-              </li>
-          </ul>
-      </div>
-      <p>Sorry for the inconvenience!</p>
-  </div>
-  <![endif]-->
-  <!-- Warning Section Ends -->
-  <!-- Required Jquery -->
-  <script type="text/javascript" src="assets/js/jquery/jquery.min.js "></script>
-  <script type="text/javascript" src="assets/js/jquery-ui/jquery-ui.min.js "></script>
-  <script type="text/javascript" src="assets/js/popper.js/popper.min.js"></script>
-  <script type="text/javascript" src="assets/js/bootstrap/js/bootstrap.min.js "></script>
-  <!-- waves js -->
-  <script src="assets/pages/waves/js/waves.min.js"></script>
-  <!-- jquery slimscroll js -->
-  <script type="text/javascript" src="assets/js/jquery-slimscroll/jquery.slimscroll.js"></script>
-  <script type="text/javascript" src="assets/js/common-pages.js"></script>
-  </body>
-  
-=======
 
     <section class="login-block">
         <!-- Container-fluid starts -->
@@ -206,13 +93,13 @@
                             <div class="card-block">
                                 <div class="row m-b-20">
                                     <div class="col-md-12">
-                                        <h3 class="text-center">Log In</h3>
+                                        <h3 class="text-center">Log In To Administrator Apotek</h3>
                                     </div>
                                 </div>
                                 <div class="form-group form-primary">
                                     <input type="text" name="email" class="form-control">
                                     <span class="form-bar"></span>
-                                    <label class="float-label">Your Email Address</label>
+                                    <label class="float-label">Email Address</label>
                                 </div>
                                 <div class="form-group form-primary">
                                     <input type="password" name="password" class="form-control">
